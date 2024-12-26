b2da9e6dfd3bf7147b75344a9a9593530ca6176f
Mahab Rizwan <mahabrizwan@gmail.com>
2024-12-26 16:12:49 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    // Add any custom webpack configurations or other settings here
+  };
+  
+  export default nextConfig;
